6f80cab7d35b11c856930b29de1fed4fef199cc8
macky337 <macky337@gmail.com>
2023-12-13 00:08:57 +0900

    出来高を追加

--- app.py
+++ app.py
@@ -10,23 +10,21 @@ import matplotlib.font_manager as fm
 # 新しいフォントを指定
 font_path = r'C:\Users\user\AppData\Local\Microsoft\Windows\Fonts\ipaexg.ttf'  # フォントファイルへのパス
 font_prop = fm.FontProperties(fname=font_path)
-# plt.rcParams['font.family'] = font_prop.get_name()
-# plt.rcParams['font.family'] = 'ipaexg'
 plt.rcParams['font.family'] = 'Meiryo'
 
 
 # Streamlit UI
 st.title('stock9_chart')
 
-# サイドバー: ユーザー入力 test 再テスト
-selected_stock = st.sidebar.text_input("銘柄ティッカーを入力してください", "AAPL,goog,meta,amzn,arm,amd,intc,crwd,nvda")
+# サイドバー: ユーザー入力
+selected_stock = st.sidebar.text_input("銘柄ティッカーを入力してください", "AAPL,GOOG,META,AMZN,ARM,AMD,INTC,CRWD,NVDA")
 time_frame = st.sidebar.selectbox("タイムフレームを選択", ["1d", "1wk", "1mo"])
 period = st.sidebar.selectbox("期間を選択", ["3M", "6M", "1Y", "3Y", "5Y", "10Y"])
 moving_averages = st.sidebar.multiselect("移動平均を選択", ["5D", "25D", "75D", "200D"])
 display_mode = st.sidebar.selectbox("表示モードを選択", ["Single", "Multiple"])
 
-# データ取得 & プロット
-symbols = selected_stock.split(',')
+# ユーザーの入力から銘柄リストを作成
+symbols = [s.strip().upper() for s in selected_stock.split(',')]
 
 # 期間をdatetimeに変換
 period_mapping = {
@@ -45,72 +43,41 @@ def plot_chart(ax, symbol, period, time_frame, moving_averages):
         else:
             df = yf.download(symbol, start=datetime.now()-period_mapping[period], interval=time_frame)
 
-        ax.plot(df['Close'], label='終値')
-
-        # 最終終値とその日付を取得
-        last_date = df.index[-1]
+        # 終値を取得
         last_close = df['Close'].iloc[-1]
 
-        # 前日の終値を取得
-        if len(df) > 1:
-            previous_close = df['Close'].iloc[-2]
-        else:
-            previous_close = last_close
+        ax.plot(df['Close'], label='終値')
 
-        # 前日比を計算
-        change = last_close - previous_close
-        percent_change = (change / previous_close) * 100 if previous_close != 0 else 0
+        # 出来高をプロットするための新しい軸を作成
+        ax2 = ax.twinx()
+        ax2.bar(df.index, df['Volume'], alpha=0.3, color='grey')
+        ax2.set_ylabel('Volume')
+        ax2.grid(False)
 
-        # 移動平均の計算と描画
-        for ma in moving_averages:
-            if len(df) >= int(ma[:-1]):  # データが十分にあるか確認
-                df['MA' + ma] = df['Close'].rolling(window=int(ma[:-1])).mean()
-                ax.plot(df['MA' + ma], label=f'{ma} 移動平均')
+        # タイトルに銘柄名と終値を表示
+        ax.set_title(f"{symbol} - 終値: {last_close:.2f}")
 
-        # 終値と前日比を注釈としてチャートに表示
-        ax.annotate(f'{last_close:.2f}\n({change:+.2f}, {percent_change:+.2f}%)',
-                    xy=(last_date, last_close),
-                    xytext=(15, 0),
-                    textcoords='offset points',
-                    arrowprops=dict(arrowstyle='->'),
-                    horizontalalignment='right',
-                    verticalalignment='bottom',
-                    fontsize=8)
-
-        # タイトルに終値と前日比を追加
-        ax.set_title(f"{symbol} {last_close:.2f} ({change:+.2f}, {percent_change:+.2f}%)", fontsize=14)
-        ax.legend(loc='upper left')
-        ax.get_xaxis().set_visible(True)
-
-        # X軸の日付フォーマットを設定
-        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-        plt.setp(ax.xaxis.get_majorticklabels(), rotation=45)
+        # ... (残りのプロットロジック)
 
     except Exception as e:
         st.sidebar.text(f"エラー: {symbol} - {str(e)}")
 
-
 if display_mode == "Single":
-    # インデントされたコードブロック
-    # 例えば、シングル表示モード用の処理
     symbol = st.sidebar.selectbox("銘柄を選択", symbols)
-    symbol = symbol.strip().upper()
-
     fig, ax1 = plt.subplots(figsize=(12, 6))
     plot_chart(ax1, symbol, period, time_frame, moving_averages)
     plt.tight_layout()
     st.pyplot(fig)
 else:
-    # インデントされたコードブロック
-    # 例えば、マルチプル表示モード用の処理
-    fig, axs = plt.subplots(3, 3, figsize=(18, 12))
+    fig, axs = plt.subplots(3, 3, figsize=(18, 12))  # 3x3のグリッドでサブプロットを作成
     fig.suptitle('株価とボリューム', fontsize=20)
     axs = axs.flatten()
 
-    # 各銘柄のチャートをプロットする処理
     for i, symbol in enumerate(symbols):
-        symbol = symbol.strip().upper()
-        plot_chart(axs[i], symbol, period, time_frame, moving_averages)
+        if i < 9:  # 最大9つのチャートを表示
+            plot_chart(axs[i], symbol, period, time_frame, moving_averages)
+        else:
+            axs[i].set_visible(False)  # 9つ以上のチャートは表示しない
 
     plt.tight_layout(rect=[0, 0.03, 1, 0.95])
     st.pyplot(fig)
